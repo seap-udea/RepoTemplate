79456ed637eea113cdaa24ab736f84a7140c7e80
Jorge Zuluaga <jzuluaga@urania.udea.edu.co>
2019-05-12 11:14:33 -0500

    Commit

---
+++ util/repo/repo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+. .reporc
+
+echo "Removing all git information..."
+mv .git .git.bak
+
+echo "Creating GitHub repository $(USER)/$(REPO) (if this is not the right user ctrl+c and run: USER=<user> make repo)"
+curl -o /dev/null -s -u '$USER' https://api.github.com/user/repos -d '{"name":"$REPO"}'
+
+echo "Initializing repo..."
+git init
+git add -A
+cmd="git remote add origin $GITMETH-$USER$GITCHAR$USER/$REPO.git"
+echo $cmd
+exit 0
+
+git commit -am "New Repo"
+echo "Pushing files..."
+git push origin master
+
+echo "#Automatically generate by make repo:" >> .reporc
+echo "REPO=$(REPO)" >> .reporc
+echo "USER=$(USER)" >> .reporc
+
+chmod +x $(UTIL)/sonar/tools/*
+make repoinit
+
