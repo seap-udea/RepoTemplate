c060a9a2bd18ec7e320ad2130598ec566db2b3e8
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2019-05-13 14:36:17 -0500

    Commit

--- util/repo/files.sh
+++ util/repo/files.sh
@@ -2,9 +2,10 @@
 . .pack/packrc
 . $REPODIR/reporc
 
+make cleancrap
 find . -type f \
     |grep -v "./.git/" \
-    |grep -E "util/repo/|util/sonar/|./.reporc" \
+    |grep -E "util/repo/|util/sonar/|./.pack" \
     |sed -e "s/.\///" \
     |tee $REPODIR/files.list |tee $REPODIR/repofiles.list
 
